6e092aa49cfb0de011167ec0eee93244abe640f5
Choaib ELMADI <choaibamd@gmail.com>
2024-10-10 22:26:17 +0100

    Prevent adding a new pin if it already exists

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import {
-	getCurrentTab,
 	handleNotDiscussionTab,
+	showPinnedDiscussions,
+	getPinnedDiscussions,
 	handleShowPinButton,
+	getCurrentTab,
 } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -53,6 +55,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 							? JSON.parse(userPinnedDiscussions)
 							: [];
 
+						const discussionExists = pinnedDiscussions.some(
+							(discussion) => discussion.id === newDiscussion.id
+						);
+
+						if (discussionExists) {
+							return;
+						}
+
 						pinnedDiscussions = [...pinnedDiscussions, newDiscussion].sort(
 							(a, b) => b.time - a.time
 						);
@@ -70,110 +80,4 @@ document.addEventListener("DOMContentLoaded", async () => {
 			);
 		});
 	});
-
-	//! ---- ---- ---- ---- ---- ---- ---- ---- ---- !//
-
-	function getPinnedDiscussions() {
-		return new Promise((resolve) => {
-			chrome.storage.sync.get("userPinnedDiscussions", (obj) => {
-				resolve(
-					obj["userPinnedDiscussions"]
-						? JSON.parse(obj["userPinnedDiscussions"])
-						: []
-				);
-			});
-		});
-	}
-
-	function showPinnedDiscussions(discussions) {
-		const discussionsContainer = document.getElementById("pinned-container");
-		console.log("Show discussions");
-
-		discussionsContainer.innerHTML = "";
-
-		if (discussions.length < 1) {
-			const emptyDiscussionsContainer = document.createElement("div");
-			emptyDiscussionsContainer.className = "empty-discussions-container";
-
-			const emptyDiscussionsContent = document.createElement("h1");
-			emptyDiscussionsContent.className = "empty-discussions-content";
-			emptyDiscussionsContent.innerText = "No pinned discussions!";
-			emptyDiscussionsContainer.appendChild(emptyDiscussionsContent);
-
-			discussionsContainer.appendChild(emptyDiscussionsContainer);
-
-			return;
-		}
-
-		discussions.forEach((discussion) => {
-			const pinnedDiscussion = document.createElement("div");
-			pinnedDiscussion.className = "pinned";
-
-			const pinnedTitle = document.createElement("p");
-			pinnedTitle.className = "pinned-title";
-			pinnedTitle.innerText = `${
-				discussion?.title ? discussion?.title : "Discussion"
-			}`;
-			pinnedDiscussion.appendChild(pinnedTitle);
-
-			const pinnedButtons = document.createElement("div");
-			pinnedButtons.className = "pinned-buttons";
-
-			const viewLink = document.createElement("a");
-			viewLink.href = `${discussion.link}`;
-			viewLink.target = "_blank";
-			viewLink.className = "view-link";
-			viewLink.title = "View discussion";
-
-			const viewLinkImage = document.createElement("img");
-			viewLinkImage.src = "Images/link_16x16.png";
-			viewLinkImage.alt = "Link";
-			viewLinkImage.draggable = "false";
-			viewLink.appendChild(viewLinkImage);
-
-			pinnedButtons.appendChild(viewLink);
-
-			const deleteBtn = document.createElement("button");
-			deleteBtn.className = "delete";
-			deleteBtn.dataset.index = discussion.id;
-			deleteBtn.title = "Delete discussion";
-			deleteBtn.addEventListener("click", function () {
-				const indexToDelete = this.dataset.index;
-				deleteDiscussionById(indexToDelete);
-			});
-
-			const deleteBtnImage = document.createElement("img");
-			deleteBtnImage.src = "Images/delete_16x16.png";
-			deleteBtnImage.alt = "Delete";
-			deleteBtnImage.draggable = "false";
-			deleteBtn.appendChild(deleteBtnImage);
-
-			pinnedButtons.appendChild(deleteBtn);
-
-			pinnedDiscussion.appendChild(pinnedButtons);
-
-			discussionsContainer.appendChild(pinnedDiscussion);
-		});
-	}
-
-	function deleteDiscussionById(id) {
-		chrome.storage.sync.get("userPinnedDiscussions", function (result) {
-			if (result.userPinnedDiscussions) {
-				let pinnedDiscussions = JSON.parse(result.userPinnedDiscussions);
-
-				pinnedDiscussions = pinnedDiscussions.filter(
-					(discussion) => discussion.id !== id
-				);
-
-				chrome.storage.sync.set(
-					{
-						userPinnedDiscussions: JSON.stringify(pinnedDiscussions),
-					},
-					function () {
-						showPinnedDiscussions(pinnedDiscussions);
-					}
-				);
-			}
-		});
-	}
 });
--- utils.js
+++ utils.js
@@ -25,3 +25,107 @@ export function handleShowPinButton() {
 	const pinNewDiscussionBtn = document.getElementById("pin-new-discussion");
 	pinNewDiscussionBtn.classList.add("show");
 }
+
+export function getPinnedDiscussions() {
+	return new Promise((resolve) => {
+		chrome.storage.sync.get("userPinnedDiscussions", (obj) => {
+			resolve(
+				obj["userPinnedDiscussions"]
+					? JSON.parse(obj["userPinnedDiscussions"])
+					: []
+			);
+		});
+	});
+}
+
+export function showPinnedDiscussions(discussions) {
+	const discussionsContainer = document.getElementById("pinned-container");
+	console.log("Show discussions");
+
+	discussionsContainer.innerHTML = "";
+
+	if (discussions.length < 1) {
+		const emptyDiscussionsContainer = document.createElement("div");
+		emptyDiscussionsContainer.className = "empty-discussions-container";
+
+		const emptyDiscussionsContent = document.createElement("h1");
+		emptyDiscussionsContent.className = "empty-discussions-content";
+		emptyDiscussionsContent.innerText = "No pinned discussions!";
+		emptyDiscussionsContainer.appendChild(emptyDiscussionsContent);
+
+		discussionsContainer.appendChild(emptyDiscussionsContainer);
+
+		return;
+	}
+
+	discussions.forEach((discussion) => {
+		const pinnedDiscussion = document.createElement("div");
+		pinnedDiscussion.className = "pinned";
+
+		const pinnedTitle = document.createElement("p");
+		pinnedTitle.className = "pinned-title";
+		pinnedTitle.innerText = `${
+			discussion?.title ? discussion?.title : "Discussion"
+		}`;
+		pinnedDiscussion.appendChild(pinnedTitle);
+
+		const pinnedButtons = document.createElement("div");
+		pinnedButtons.className = "pinned-buttons";
+
+		const viewLink = document.createElement("a");
+		viewLink.href = `${discussion.link}`;
+		viewLink.target = "_blank";
+		viewLink.className = "view-link";
+		viewLink.title = "View discussion";
+
+		const viewLinkImage = document.createElement("img");
+		viewLinkImage.src = "Images/link_16x16.png";
+		viewLinkImage.alt = "Link";
+		viewLinkImage.draggable = "false";
+		viewLink.appendChild(viewLinkImage);
+
+		pinnedButtons.appendChild(viewLink);
+
+		const deleteBtn = document.createElement("button");
+		deleteBtn.className = "delete";
+		deleteBtn.dataset.index = discussion.id;
+		deleteBtn.title = "Delete discussion";
+		deleteBtn.addEventListener("click", function () {
+			const indexToDelete = this.dataset.index;
+			deleteDiscussionById(indexToDelete);
+		});
+
+		const deleteBtnImage = document.createElement("img");
+		deleteBtnImage.src = "Images/delete_16x16.png";
+		deleteBtnImage.alt = "Delete";
+		deleteBtnImage.draggable = "false";
+		deleteBtn.appendChild(deleteBtnImage);
+
+		pinnedButtons.appendChild(deleteBtn);
+
+		pinnedDiscussion.appendChild(pinnedButtons);
+
+		discussionsContainer.appendChild(pinnedDiscussion);
+	});
+}
+
+function deleteDiscussionById(id) {
+	chrome.storage.sync.get("userPinnedDiscussions", function (result) {
+		if (result.userPinnedDiscussions) {
+			let pinnedDiscussions = JSON.parse(result.userPinnedDiscussions);
+
+			pinnedDiscussions = pinnedDiscussions.filter(
+				(discussion) => discussion.id !== id
+			);
+
+			chrome.storage.sync.set(
+				{
+					userPinnedDiscussions: JSON.stringify(pinnedDiscussions),
+				},
+				function () {
+					showPinnedDiscussions(pinnedDiscussions);
+				}
+			);
+		}
+	});
+}
